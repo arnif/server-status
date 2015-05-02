3e8f3d3f8340277759fb576182aec5dc291d2249
arnif <arnifa@gmail.com>
2015-05-02 20:25:04 +0000

    cpu usage

--- main.py
+++ main.py
@@ -17,13 +17,8 @@ def getCPUTemp():
 
 @app.route("/api/server/cpu")
 def getMEMUsage():
-    cpuUsage = subprocess.check_output("top -b -n 2 -d .5 | grep Cpu", shell=True)
-    cpus = cpuUsage.split('Cpu(s):')
-    for c in cpus:
-        total = c[0:4]
-    print total
-    return total
-
+    cpuUsage = subprocess.check_output("top -d 0.5 -b -n2 | grep 'Cpu(s)'|tail -n 1 | awk '{print $2 + $4}'", shell=True)
+    return cpuUsage
 
 if __name__ == "__main__":
      app.run(host='0.0.0.0', port=8081)
