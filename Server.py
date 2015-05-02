1b911717fa1b4c97a2589f0606f1029f7fbabbe3
arnif <arnifa@gmail.com>
2015-05-02 23:17:06 +0000

    all under one path

--- Server.py
+++ Server.py
@@ -4,20 +4,42 @@ import os, sys, json, math
 class Server:
 
     @staticmethod
-    def test(fo):
-        return fo
-
-    @staticmethod
     def getCPUtemp():
-        temp = getSuprocessOutput("sensors | grep 'Core 1'")[17:21]
+	temp = subprocess.check_output("sensors | grep 'Core 1'", shell=True)[17:21]
         return dict(temperature=temp)
 
     @staticmethod
     def getCPUusage():
-        cpuUsage = getSuprocessOutput("top -d 0.5 -b -n2 | grep 'Cpu(s)'|tail -n 1 | awk '{print $2 + $4}'").rstrip('\n')
+        cpuUsage = subprocess.check_output("top -d 0.5 -b -n2 | grep 'Cpu(s)'|tail -n 1 | awk '{print $2 + $4}'", shell=True).rstrip('\n')
         return dict(usage=cpuUsage)
 
+    @staticmethod
+    def getMemInfo():
+	memtotal = subprocess.check_output("cat /proc/meminfo | grep MemTotal | sed 's/ //g'", shell=True)[9:16]
+     	memfree = subprocess.check_output("cat /proc/meminfo | grep MemFree | sed 's/ //g'", shell=True)[8:14]
+	return dict(total=memtotal, free=memfree)
+    
+    @staticmethod
+    def getHDDinfo():
+	bak = disk_usage("/media/bak")
+   	one = disk_usage("/media/#1")
+   	c = disk_usage("/")
+	return [bak, one, c]
+
+    @staticmethod
+    def getUptime():
+	up_time = subprocess.check_output("cut -d. -f1 /proc/uptime", shell=True).rstrip('\n')
+   	up_time = float(up_time)
+    	days = int(math.floor(up_time / 60 / 60 / 24))
+    	hrs = int(up_time / 60 / 60 % 24)
+    	mins = int(up_time / 60 % 60)
+    	secs = int(up_time % 60)
+	return dict(days=days, hours=hrs, mins=mins, secs=secs)
 
 
-    def getSuprocessOutput(command):
-        return subprocess.check_output(command, shell=True)
+def disk_usage(path):
+    st = os.statvfs(path)
+    free = st.f_bavail * st.f_frsize
+    total = st.f_blocks * st.f_frsize
+    used = (st.f_blocks - st.f_bfree) * st.f_frsize
+    return dict(hdd=path, free=free, total=total, used=used)
--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 from flask import Flask
 from flask.ext.cors import CORS
 from flask import jsonify
-from collections import namedtuple
 import subprocess
-import os, sys, json, math
 from Server import *
 app = Flask(__name__)
 cors = CORS(app)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
 @app.route("/api/server")
 def getAll():
-    f = Server.test(22)
-    print type(f)
-    return jsonify(number=f)
+    cpu = Server.getCPUtemp().copy()
+    cpu.update(Server.getCPUusage())
+    memory = Server.getMemInfo()
+    hdd = Server.getHDDinfo() 
+    up = Server.getUptime()
+    return jsonify(cpu=cpu, memory=memory, harddrives=hdd, uptime=up)
 
 @app.route("/api/server/temp")
 def getCPUTemp():
@@ -26,43 +23,24 @@ def getCPUTemp():
 
 @app.route("/api/server/cpu")
 def getCPUUsage():
-    cpuUsage = subprocess.check_output("top -d 0.5 -b -n2 | grep 'Cpu(s)'|tail -n 1 | awk '{print $2 + $4}'", shell=True).rstrip('\n')
-    return jsonify(usage=cpuUsage)
+    usage = Server.getCPUusage()
+    return jsonify(usage)
 
 @app.route("/api/server/memory")
 def getMEMStatus():
-    memtotal = subprocess.check_output("cat /proc/meminfo | grep MemTotal | sed 's/ //g'", shell=True)[9:16]
-    memfree = subprocess.check_output("cat /proc/meminfo | grep MemFree | sed 's/ //g'", shell=True)[8:14]
-    return jsonify(total=memtotal, free=memfree)
+    memory = Server.getMemInfo()
+    return jsonify(memory)
 
 @app.route("/api/server/hdd")
 def getHDDStatus():
-    bak = disk_usage("/media/bak")
-    one = disk_usage("/media/#1")
-    c = disk_usage("/")
-    return jsonify(harddrives=[bak, one, c])
+    hdd = Server.getHDDinfo()
+    return jsonify(harddrives=hdd)
 
 @app.route("/api/server/uptime")
 def getUptime():
-    up_time = subprocess.check_output("cut -d. -f1 /proc/uptime", shell=True).rstrip('\n')
-    up_time = float(up_time)
-    days = int(math.floor(up_time / 60 / 60 / 24))
-    hrs = int(up_time / 60 / 60 % 24)
-    mins = int(up_time / 60 % 60)
-    secs = int(up_time % 60)
-    return jsonify(days=days, hours=hrs, mins=mins, secs=secs)
+    uptime = Server.getUptime()
+    return jsonify(uptime)
 
-def disk_usage(path):
-    """Return disk usage statistics about the given path.
-
-    Returned valus is a named tuple with attributes 'total', 'used' and
-    'free', which are the amount of total, used and free space, in bytes.
-    """
-    st = os.statvfs(path)
-    free = st.f_bavail * st.f_frsize
-    total = st.f_blocks * st.f_frsize
-    used = (st.f_blocks - st.f_bfree) * st.f_frsize
-    return dict(hdd=path, free=free, total=total, used=used)
 
 if __name__ == "__main__":
      app.run(host='0.0.0.0', port=8081)
