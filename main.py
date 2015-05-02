dee4d670b5f2c502ee685f8fd4397c9259b2b289
arnif <arnifa@gmail.com>
2015-05-02 00:32:50 +0000

    cpu

--- main.py
+++ main.py
@@ -15,6 +15,15 @@ def getCPUTemp():
     return jsonify(temperature=temp)
 
 
+@app.route("/api/server/cpu")
+def getMEMUsage():
+    cpuUsage = subprocess.check_output("top -b -n 2 -d .5 | grep Cpu", shell=True)
+    cpus = cpuUsage.split('Cpu(s):')
+    for c in cpus:
+        total = c[0:4]
+    print total
+    return total
+
 
 if __name__ == "__main__":
      app.run(host='0.0.0.0', port=8081)
